10e69e3f20f83dd36cefdf3004c9dfece0b9f9ac
Emilia Hernandez <emilia.b.hernandez@gmail.com>
2020-06-03 00:09:40 -0400

    Began Project
    Working player, camera follows, basic art, colliders, the basics really

---
+++ livemassbaybee/Assets/ani/CharacterAnimation.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimation : MonoBehaviour {
+
+    private Animator anim;
+
+    void Start(){
+        anim = GetComponent<Animator>();
+    }
+
+    void Update(){
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            anim.SetBool("iswalkingleft", true);
+            anim.SetBool("iswalkingdown", false);
+            anim.SetBool("iswalkingright", false);
+            anim.SetBool("iswalkingup", false);
+        }
+        else
+        {
+            anim.SetBool("iswalkingleft", false);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            anim.SetBool("iswalkingright", true);
+            anim.SetBool("iswalkingup", false);
+            anim.SetBool("iswalkingleft", false);
+            anim.SetBool("iswalkingdown", false);
+        }
+        else
+        {
+            anim.SetBool("iswalkingright", false);
+        }
+
+    }
+}
